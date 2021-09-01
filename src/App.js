7beb0fa2b532a6267eb8e6c08f8d45e9c07167a2
Sergey23131 <semno321@gmail.com>
2021-09-02 00:47:27 +0300

    module

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
+import {discoverMovie} from "./services/services";
+import {useDispatch, useSelector} from "react-redux";
+import {loadMovies} from "./redux/functionsOfActions";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+    let state = useSelector(state => state);
+    let dispatch = useDispatch()
+    let {movies}=state
+    console.log(state)
+
+    useEffect(() => {
+        discoverMovie().then(value => dispatch(loadMovies(value)))
+    }, [])
+    return (
+        <div className="App">
+            {movies.map(value => <div>{value.title}</div>)}}
+        </div>
+    );
 }
 
-export default App;
+
---
+++ src/redux/reducer.js
@@ -0,0 +1,9 @@
+import {LOAD_MOVIES} from "./actions";
+
+let initialState = {movies: []};
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case LOAD_MOVIES:
+            return {...state, movies: [...action.payload.data.results]}
+    }
+}
