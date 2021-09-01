fed28f75da7da7cd134dcfd276d6c0e379de6916
Sergey23131 <semno321@gmail.com>
2021-09-01 17:37:38 +0300

    redux2.1

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
+
+
+
+
+const store= createStore(reducer)
 
 ReactDOM.render(
   <React.StrictMode>
