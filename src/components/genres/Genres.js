52c2f2f12d88eb583833f0520fb1c37986c34f40
Sergey23131 <semno321@gmail.com>
2021-09-03 19:29:33 +0300

    module

---
+++ src/components/genres/Genres.js
@@ -0,0 +1,18 @@
+export default function Genres({item, info}) {
+    let genresList;
+
+/* let a = () => {
+
+
+        genresList = item.map(list => info.genre_ids.find(obj => obj.id === list.id))
+    }
+
+    a();*/
+    console.log(genresList)
+
+    return (
+        <div className="Genres">
+
+        </div>
+    );
+}
