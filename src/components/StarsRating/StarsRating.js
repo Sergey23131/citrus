93b998b82ad796bccdd033a9f03171cb03fc3d80
Sergey23131 <semno321@gmail.com>
2021-09-04 23:07:08 +0300

    module

--- src/components/StarsRating/StarsRating.js
+++ src/components/StarsRating/StarsRating.js
@@ -1,20 +1,22 @@
+import
+
 export default function StarsRating({item}) {
 
     return (
         <div className="StarsRating">
-            <div className={'rating_body'}>
-                <div className={'rating_active'}>
+            <div className={'Rating_body'}>
+                <div className={'Rating_active'}>
                 </div>
-                <div className={'rating_items'}>
-                    <input type={"radio"} className={'rating_item'} value={'1'} name={'rating'}/>
-                    <input type={"radio"} className={'rating_item'} value={'2'} name={'rating'}/>
-                    <input type={"radio"} className={'rating_item'} value={'3'} name={'rating'}/>
-                    <input type={"radio"} className={'rating_item'} value={'4'} name={'rating'}/>
-                    <input type={"radio"} className={'rating_item'} value={'5'} name={'rating'}/>
+                <div className={'Rating_items'}>
+                    <input type={"radio"} className={'Rating_item'} value={'1'} name={'rating'}/>
+                    <input type={"radio"} className={'Rating_item'} value={'2'} name={'rating'}/>
+                    <input type={"radio"} className={'Rating_item'} value={'3'} name={'rating'}/>
+                    <input type={"radio"} className={'Rating_item'} value={'4'} name={'rating'}/>
+                    <input type={"radio"} className={'Rating_item'} value={'5'} name={'rating'}/>
                 </div>
             </div>
 
-            <div className={'rating_value'}>{item.vote_average}</div>
+            <div className={'Rating_value'}>{item.vote_average}</div>
         </div>
     );
 }
